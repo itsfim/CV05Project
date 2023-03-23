0d6172f088b6fd66c622d74f2f8ce91d6426c02f
Abbie0197 <56072349+Abbie0197@users.noreply.github.com>
2023-03-23 22:45:21 +0000

    Update Report.js

--- src/components/Report/Report.js
+++ src/components/Report/Report.js
@@ -7,7 +7,20 @@ class Report extends React.Component {
             <div className='pageIntro'>
                 <h1 className='h1'>Report Page</h1>
                 <main className='main'>
+                    <php
+                	$sql = "SELECT * FROM GameScore WHERE userID = '1' ORDER BY Dateplayed";
+                	$result = $db.query($sql);
 
+                	if ($result->num_rows > 0) {
+  						echo "<table><tr><th>ID</th><th>Name</th></tr>";
+    					while($row = $result->fetch_assoc()) {
+   								 echo "<tr><td>".$row["UserID"]."</td><td>".$row["Dateplayed"]." ".$row["TimeScore"]."</td></tr>";
+ 						 }
+  						echo "</table>";
+                	} else {
+  						echo "0 results";
+						}
+						?>
                 </main>
             </div>
         )
