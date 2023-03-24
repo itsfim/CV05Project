640a6df5fe449f4ebee479493bb0a99ccb440820
fahphufah <126884213+fahphufah@users.noreply.github.com>
2023-03-24 14:52:58 +0000

    Add files via upload

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -5,22 +5,20 @@ class Account extends Component {
 
     render() {
         return(
-            <div className='pageIntro'>
+            <div className='pageIn'>
                 <h1 className='h0'>Account</h1>
-            <div className='WrapperAcc'>
-                <main className='item'>
+                <main className='main'>
                 <div class="homePageDiv1" id='imageHomePage'>
                     <h2 className='subHeading'>image placeholder</h2>
                 </div>
-                <div classname='item'>
+                <div classname='accInfo'>
                     <h3 classname='subHeading1'>Account Info</h3>
                     <h3 classname='subHeading2'>Username</h3>
                     <h3 classname='subHeading3'>No. of games played</h3>
                 </div>
-                <button className="primary">EDIT</button>
-                <button className="secondary">Delete Account</button>
+                <button className="primary1">EDIT</button>
+                <button className="secondary1">Delete Account</button>
                 </main>
-            </div>
             </div>
         )
     }
