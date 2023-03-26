520cb0459b309c1829334e1c9fb253c7c1570157
Fimsta <102530260+Fimsta@users.noreply.github.com>
2023-03-26 16:43:56 +0100

    Update Timer.js

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,4 +1,9 @@
 import React from 'react';
+/**
+ * @Author Daniel Fimister
+ * This script provides the current date and time as of when it is initialised
+ * it is intended for this to be used to measure when a user session starts
+ */
 class Timer extends React.Component {
     
     render() {
